be40d8d8d14841989e225aea0d63ec4b97eaab90
ckarakoc <c.karakoc@live.nl>
2024-05-21 11:07:42 +0200

    Auth with supabase completed.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, NgZone } from '@angular/core';
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { environment } from "../../environments/environment.development";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private supabase: SupabaseClient;
+  private router = inject(Router);
+  private _ngZone = inject(NgZone);
 
   constructor() {
     this.supabase = createClient(
@@ -17,7 +20,20 @@ export class AuthService {
     this.supabase.auth.onAuthStateChange((event, session) => {
       console.log("event", event);
       console.log("session", session);
+
+      localStorage.setItem('session', JSON.stringify(session?.user));
+
+      if (session?.user) {
+        this._ngZone.run(() => {
+          this.router.navigate(['/chat']);
+        });
+      }
     });
+  }
+
+  get isLoggedIn(): boolean {
+    const user = localStorage.getItem('session') as string;
+    return user === 'undefined' ? false : true;
   }
 
   async signInWithGoogle() {
